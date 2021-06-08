eb22cd33b6bc84b488fb4aafab4c6c98ea61182a
Nils Erik Persson <npersson001@gmail.com>
2021-06-07 23:21:42 -0400

    adding auth back in

--- src/main/java/com/personal/creaturekeeper/config/SecurityConfiguration.java
+++ src/main/java/com/personal/creaturekeeper/config/SecurityConfiguration.java
@@ -1,34 +1,34 @@
-//package com.personal.creaturekeeper.config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .csrf().disable()
-//                .authorizeRequests().anyRequest().authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth,
-//            @Value("${auth.local.user}") String user,
-//            @Value("${auth.local.password}") String password,
-//            @Value("${auth.local.role}") String role) throws Exception {
-//
-//        auth.inMemoryAuthentication()
-//                .withUser(user)
-//                .password(String.format("{noop}%s", password))
-//                .roles(role);
-//    }
-//
-//}
+package com.personal.creaturekeeper.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth,
+            @Value("${auth.local.user}") String user,
+            @Value("${auth.local.password}") String password,
+            @Value("${auth.local.role}") String role) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser(user)
+                .password(String.format("{noop}%s", password))
+                .roles(role);
+    }
+
+}
