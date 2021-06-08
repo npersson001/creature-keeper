44d4ab7958de34412ad8a24a57ad360b99401004
Nils Erik Persson <npersson001@gmail.com>
2021-06-07 21:58:19 -0400

    encrypting local test data

--- db/creature_keeper_db/local/V2.0__local_data.sql
+++ db/creature_keeper_db/local/V2.0__local_data.sql
@@ -1,5 +1,5 @@
 -- some example creatures added to local db for testing
 insert into creature_keeper_db.creatures (`name`, `species`, `age`) VALUES
-('neville', 'mustela erminea', 3),
-('henry', 'testudo graeca', 80),
-('dillon', 'lutra', 2);
+('neville', 'd+srx02w/hhaHTHzNrlCCheokmjtg0mU', 3),
+('henry', '8X933mCZ1IT3c0iWiVJhuekvbtdeTKeu', 80),
+('dillon', 'aw5fnYHqctDMkfhEB79dsg==', 2);
